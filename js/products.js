6545b26b0539ab1131b98e28b1d5b0c5a472c959
donatto22 <ottanod22@gmail.com>
2026-04-21 21:53:29 -0500

    Clase1 - Modulo 4
    Fetch a los productos

---
+++ js/products.js
@@ -0,0 +1,62 @@
+const API = 'https://dummyjson.com/products'
+
+// obteniendo los productos
+const getProducts = async () => {
+    const consulta = await fetch(API)
+    const datos = await consulta.json()
+    console.log(datos)
+    return datos
+}
+
+// obteniendo el lugar donde vamos a colocar los productos
+const productsList = document.querySelector('#productsList')
+
+// creando el producto
+const createProduct = (product) => {
+    const productDiv = document.createElement('div')
+    productDiv.classList.add('product')
+
+    const productImage = document.createElement('img')
+    productImage.classList.add('productImage')
+    productImage.src = product.thumbnail
+
+    // descripción del producto
+    const productDescription = document.createElement('div')
+
+    const name = document.createElement('p')
+    name.textContent = product.title
+
+    const category = document.createElement('div')
+    category.classList.add('productCategory')
+    category.textContent = `${product.category.charAt(0).toUpperCase() + product.category.slice(1)}`
+
+    const price = document.createElement('p')
+    price.textContent = `$ ${product.price}`
+
+    const addToCartButton = document.createElement('button')
+    addToCartButton.textContent = 'Añadir al carrito'
+
+    // agregandop la descripción
+    productDescription.appendChild(name)
+    productDescription.appendChild(category)
+    productDescription.appendChild(price)
+    productDescription.appendChild(addToCartButton)
+
+    // llenamos el div del producto
+    productDiv.appendChild(productImage)
+    productDiv.appendChild(productDescription)
+
+    return productDiv
+}
+
+// colocando el producto en la lista
+const renderProducts = async () => {
+    const data = await getProducts()
+
+    data.products.forEach(producto => {
+        const productDiv = createProduct(producto)
+        productsList.appendChild(productDiv)
+    })
+}
+
+renderProducts()
